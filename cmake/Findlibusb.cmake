72ba15b0aaa9d7b04bd0057e779338710de1f78a
psylo-dev <davenplay@gmail.com>
2024-11-17 11:41:59 +0100

    Update Findlibusb.cmake
    Termux paths

--- cmake/Findlibusb.cmake
+++ cmake/Findlibusb.cmake
@@ -2,9 +2,12 @@ find_path(LIBUSB_INCLUDE_DIR
     NAMES
         libusb.h
     PATHS
-        /usr/local/include
-        /opt/local/include
-        /usr/include
+        /data/data/com.termux/files/usr/include
+        /data/data/com.termux/files/usr/local/include
+        /data/data/com.termux/files/usr/opt/include
+        /data/data/com.termux/files/usr/libexec
+        /data/data/com.termux/files/usr/share/include
+        /data/data/com.termux/files/usr/var/include
     PATH_SUFFIXES
         libusb-1.0
 )
@@ -12,18 +15,21 @@ find_path(LIBUSB_INCLUDE_DIR
 if (libusb_USE_STATIC_LIBS AND NOT MSVC)
     set (LIBUSB_LIB_PREFIX "lib" CACHE INTERNAL "libusb library name prefix passed to find_library")
     set (LIBUSB_LIB_SUFFIX ".a" CACHE INTERNAL "libusb library name suffix passed to find_library")
-else (libusb_USE_STATIC_LIBS AND NOT MSVC)
+else()
     set (LIBUSB_LIB_PREFIX "" CACHE INTERNAL "libusb library name prefix passed to find_library")
     set (LIBUSB_LIB_SUFFIX "" CACHE INTERNAL "libusb library name suffix passed to find_library")
-endif (libusb_USE_STATIC_LIBS AND NOT MSVC)
+endif()
 
 find_library(LIBUSB_LIBRARY
     NAMES
         ${LIBUSB_LIB_PREFIX}usb-1.0${LIBUSB_LIB_SUFFIX} ${LIBUSB_LIB_PREFIX}usb${LIBUSB_LIB_SUFFIX}
     PATHS
-        /usr/local/lib
-        /opt/local/lib
-        /usr/lib
+        /data/data/com.termux/files/usr/lib
+        /data/data/com.termux/files/usr/local/lib
+        /data/data/com.termux/files/usr/opt/lib
+        /data/data/com.termux/files/usr/libexec
+        /data/data/com.termux/files/usr/share/lib
+        /data/data/com.termux/files/usr/var/lib
 )
 
 include(FindPackageHandleStandardArgs)
@@ -33,4 +39,4 @@ if (LIBUSB_FOUND)
     set(LIBUSB_INCLUDE_DIRS ${LIBUSB_INCLUDE_DIR})
     set(LIBUSB_LIBRARIES ${LIBUSB_LIBRARY})
     mark_as_advanced(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
-endif (LIBUSB_FOUND)
+endif()
